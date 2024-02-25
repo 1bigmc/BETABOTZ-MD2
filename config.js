9860f063911a07a50f04ecd093e8f1ec4c978913
1bigmc <160817427+1bigmc@users.noreply.github.com>
2024-02-25 04:16:46 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
-global.owner = ['6285842647866']  
-global.mods = ['6285842647866'] 
-global.prems = ['6285842647866']
-global.nameowner = 'Lann'
-global.numberowner = '6285842647866'
+global.owner = ['559285013050']  
+global.mods = ['559285013050'] 
+global.prems = ['559285013050']
+global.nameowner = 'big-mč'
+global.numberowner = '559285013050'
 global.mail = 'support@tioprm.eu.org' 
 global.gc = 'https://chat.whatsapp.com/G4f1fTpz9zL4EH3FyIcaPR'
-global.instagram = 'https://instagram.com/erlanrahmat_14'
+global.instagram = 'https://instagram.com/1bigmc'
 global.wm = '© Tio'
 global.wait = '_*Tunggu sedang di proses...*_'
 global.eror = '_*Server Error*_'
